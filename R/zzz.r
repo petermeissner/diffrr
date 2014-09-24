b7d2d81008dcfaa4eab56ada3787caa9276e5620
petermeissner <retep.meissner@gmail.com>
2014-09-24 11:55:43 +0200

    turn off unused .onLoad and .onAttach functions

--- R/zzz.r
+++ R/zzz.r
@@ -1,9 +1,9 @@
-#' description
-#'
+#' This is the PLace to put OnLoad and OnAttach functions.
+#' ... these are turned off
 
-.onLoad   <- function(libname, pkgname){
-  packageStartupMessage("Uh, Package Loaded!")
-}
-.onAttach   <- function(libname, pkgname){
-  packageStartupMessage("Package Kind of Attached - I think.")
-}
+#  .onLoad   <- function(libname, pkgname){
+#    packageStartupMessage("Uh, Package Loaded!")
+#  }
+#  .onAttach   <- function(libname, pkgname){
+#    packageStartupMessage("Package Kind of Attached - I think.")
+#  }
